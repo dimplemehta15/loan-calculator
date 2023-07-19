b0b918ce63577a7d190b488670da0c34de1662ac
dimplemehta153 <dimplemehta.15.3@gmail.com>
2023-07-19 20:34:21 +0100

    Testing Git command - 3

--- src/main/java/com/mycompany/loan/controller/LoanController.java
+++ src/main/java/com/mycompany/loan/controller/LoanController.java
@@ -9,7 +9,7 @@ public class LoanController {
 
     @GetMapping(value = "/v1/api/calculate-loan-amount")
     public Double calculateLoanAmount(@RequestParam(value="desiredAmount") Double principal, @RequestParam(value = "paybackTime") Float numberOfYears) {
-        System.out.println("Testing Git command - 1");
+        System.out.println("Testing Git command - 3");
         return principal * numberOfYears;
     }
 }
